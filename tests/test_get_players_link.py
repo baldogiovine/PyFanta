717ea45d2ea0527968e0082a0e1ec8f002f276d2
baldogiovine <federicojacopo.baldoni@gmail.com>
2025-01-16 18:35:58 +0100

    TST: add second test

--- tests/test_get_players_link.py
+++ tests/test_get_players_link.py
@@ -3,8 +3,33 @@
 from unittest.mock import patch
 
 import pytest
+from aioresponses import aioresponses
 
 from src.scraper.get_players_links import GetPlayersLinks
+
+
+@pytest.fixture  # type: ignore
+def year() -> str:
+    """Pytest fixture to pass "year" parameter to GetPlayersLinks class."""
+    return "2024-25"
+
+
+@pytest.fixture  # type: ignore
+def url(year: str) -> str:
+    """Pytest fixture to construct the URL for fetching player links.
+
+    Parameters
+    ----------
+    year : str
+        The year for which player links are to be fetched.
+
+    Returns:
+    -------
+    str
+        The constructed URL for the specified year.
+    """
+    scraper = GetPlayersLinks(year)
+    return scraper._construct_url()
 
 
 @pytest.mark.parametrize(  # type: ignore
@@ -23,3 +48,44 @@ def test_construct_url(year: str, expected_url: str) -> None:
     ):
         scraper = GetPlayersLinks(year=year)
         assert scraper._construct_url() == expected_url
+
+
+@pytest.mark.asyncio  # type: ignore
+async def test_get_links_success(year: str, url: str) -> None:
+    """Test that get_links successfully extracts player names and links."""
+    mocked_html = """
+    <div class="container">
+        <div class="table-overflow">
+            <table>
+                <tr class="player-row">
+                    <th class="player-name">
+                        <a class="player-name player-link" href="https://www.fantacalcio.it/serie-a/squadre/atalanta/lookman/4730">
+                        <span>Lookman</span>
+                        <a class="player-name player-link" href="https://www.fantacalcio.it/serie-a/squadre/milan/pulisic/2423">
+                            <span>Pulisic</span>
+                        </a>
+                    </th>
+                </tr>
+            </table>
+        </div>
+    </div>
+    """
+
+    with aioresponses() as mocked:
+        mocked.get(url, status=200, body=mocked_html)
+
+        scraper = GetPlayersLinks(year)
+        result = await scraper.get_links()
+
+        number_results = 2
+        assert len(result) == number_results
+        assert result[0]["name"] == "Lookman"
+        assert (
+            result[0]["link"]
+            == "https://www.fantacalcio.it/serie-a/squadre/atalanta/lookman/4730/2024-25"
+        )
+        assert result[1]["name"] == "Pulisic"
+        assert (
+            result[1]["link"]
+            == "https://www.fantacalcio.it/serie-a/squadre/milan/pulisic/2423/2024-25"
+        )
